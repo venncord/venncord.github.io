15f81ce752f40212cef8a4a83c9273a16f01fbdf
venn <119350695+venncord@users.noreply.github.com>
2024-03-03 19:28:12 +0000

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var blueLight = document.getElementById("blue-light");
-  document.addEventListener("mousemove", function(event) {
-    var mouseX = event.clientX;
-    var mouseY = event.clientY;
-    blueLight.style.left = mouseX + "px";
-    blueLight.style.top = mouseY + "px";
-  });
-});
